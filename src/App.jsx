ed3ec7bdcc0289dc09c174a0886278f3fb8a99c3
LongDoan09 <ddoan3@charlotte.edu>
2025-11-30 14:58:40 -0500

    fix app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Header from "./header.jsx";
+import Footer from "./footer.jsx";
+import Home from "./home.jsx";
+import Introduction from "./introduction.jsx";
+import Contract from "./contract.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/introduction" element={<Introduction />} />
+          <Route path="/contract" element={<Contract />} />
+        </Routes>
+      </main>
+
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
--- src/header.jsx
+++ src/header.jsx
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom";
 
-export default function Header() {
+function Header() {
   return (
     <header>
       <h1>Long Doan's Loyal Donkey || ITIS 3135</h1>
-
       <nav>
-        <Link to="/">Home</Link>
-        {" || "}
-        <Link to="/introduction">Introduction</Link>
-        {" || "}
+        <Link to="/">Home</Link> ||
+        <Link to="/introduction">Introduction</Link> ||
         <Link to="/contract">Contract</Link>
       </nav>
     </header>
   );
 }
+
+export default Header;
+
